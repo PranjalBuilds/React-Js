862ca7fd21d9f92238708b4fb7e7a7a7b324267c
PranjalBuilds <imcae22060@gmail.com>
2024-06-18 21:43:47 +0530

    React-Basics

---
+++ src/Components/Demo.js
@@ -0,0 +1,10 @@
+function Demo () {
+    return (
+        <div className="card">
+            <h3>This is Component</h3>
+            <p>This is demo paragraph text</p>
+        </div>
+    );
+}
+
+export default Demo;
